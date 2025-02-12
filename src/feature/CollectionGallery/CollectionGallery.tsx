3680acffd9f6fe0e4fd1959d4b88d9b6dee92e8b
OxY <73963793+OxY623@users.noreply.github.com>
2025-02-12 15:43:48 +0300

    fix: optimization

--- src/feature/CollectionGallery/CollectionGallery.tsx
+++ src/feature/CollectionGallery/CollectionGallery.tsx
@@ -12,7 +12,6 @@ const CollectionGallery: FC<Props> = ({ classname, children }) => {
   const [data] = useState<CardData[]>([...collectionCardsData]);
   const [dataBigCard, setBigCard] = useState<CardData>(data[0]);
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [scrollLeft, setScrollLeft] = useState(0); // Добавим состояние для scrollLeft
   const scrollRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -21,23 +20,17 @@ const CollectionGallery: FC<Props> = ({ classname, children }) => {
     }
   }, [selectedIndex]);
 
-  useEffect(() => {
-    if (scrollRef.current) {
-      setScrollLeft(scrollRef.current.scrollLeft);
-    }
-  }, [scrollRef?.current?.scrollLeft]); // Слежение за изменениями scrollLeft
-
   const nextImage = () => {
     if (scrollRef.current) {
       scrollRef.current.scrollLeft += 194; // Adjust the scroll offset as needed
-      setSelectedIndex((prevIndex) => prevIndex + 1); // Увеличиваем индекс
+      setSelectedIndex((prevIndex) => prevIndex + 1); // Increment index
     }
   };
 
   const prevImage = () => {
     if (scrollRef.current) {
       scrollRef.current.scrollLeft -= 194; // Adjust the scroll offset as needed
-      setSelectedIndex((prevIndex) => prevIndex - 1); // Уменьшаем индекс
+      setSelectedIndex((prevIndex) => prevIndex - 1); // Decrement index
     }
   };
 
@@ -45,23 +38,16 @@ const CollectionGallery: FC<Props> = ({ classname, children }) => {
     setBigCard(data.find((item) => item.count === id) || data[0]);
   };
 
-  const isPrevButtonDisabled = scrollLeft === 0; // Логика для кнопки "Prev"
   const isNextButtonDisabled =
-    scrollLeft >=
-    (scrollRef.current?.scrollWidth || 0) -
-      (scrollRef.current?.clientWidth || 0); // Логика для кнопки "Next"
+    scrollRef.current?.scrollWidth &&
+    scrollRef.current?.scrollLeft ===
+      scrollRef?.current?.scrollWidth - scrollRef?.current?.clientWidth;
+
+  const isPrevButtonDisabled = scrollRef.current?.scrollLeft === 0;
 
   return (
     <div className={`mx-auto flex items-end gap-4 p-4 ${classname}`}>
       <div className="relative flex flex-col xl:flex-row">
-        <div className="absolute inset-0 z-[-10] hidden h-[317px] w-[248px] flex-wrap gap-[8px] xl:flex">
-          {[...Array(16 * 20)].map((_, index) => (
-            <div
-              key={index}
-              className="h-[8px] w-[8px] bg-grey opacity-40"
-            ></div>
-          ))}
-        </div>
         {data.length > 0 && (
           <div className="relative px-[65px] pt-[50px]" key={dataBigCard.id}>
             <Card key={dataBigCard.id} cardData={dataBigCard} size={Size.BIG} />
@@ -102,23 +88,17 @@ const CollectionGallery: FC<Props> = ({ classname, children }) => {
             <div className="h-full overflow-hidden">
               <div
                 ref={scrollRef}
-                className="mb-[-17px] flex w-[169px] space-x-[30px] overflow-x-scroll scroll-smooth mobile:w-[369px] md:w-[560px]"
-                onScroll={() =>
-                  setScrollLeft(scrollRef.current?.scrollLeft || 0)
-                } // Обновляем scrollLeft при прокрутке
+                className="mb-[-50px] flex w-[169px] space-x-[30px] overflow-x-scroll scroll-smooth mobile:w-[369px] md:w-[560px]"
               >
-                {data &&
-                  data.map((item) => {
-                    return (
-                      <div className="" key={item.id}>
-                        <Card
-                          cardData={item}
-                          size={Size.SMALL}
-                          onClickButton={() => handleClickButton(item.count)}
-                        />
-                      </div>
-                    );
-                  })}
+                {data.map((item) => (
+                  <div key={item.id}>
+                    <Card
+                      cardData={item}
+                      size={Size.SMALL}
+                      onClickButton={() => handleClickButton(item.count)}
+                    />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
@@ -127,4 +107,5 @@ const CollectionGallery: FC<Props> = ({ classname, children }) => {
     </div>
   );
 };
+
 export { CollectionGallery };
